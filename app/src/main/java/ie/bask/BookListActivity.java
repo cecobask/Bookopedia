d9e4a92f024b254e5a6a6fdfef1bc9e1a02f97fc
cecobask <32335835+cecobask@users.noreply.github.com>
2019-02-02 21:50:02 +0000

    Added SearchView, querying for books based on user input. Progress bar shows while loading results. Changed app icon.

--- app/src/main/java/ie/bask/BookListActivity.java
+++ app/src/main/java/ie/bask/BookListActivity.java
@@ -2,12 +2,14 @@ package ie.bask;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,27 +18,50 @@ import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
+
 public class BookListActivity extends AppCompatActivity {
 
     private ListView lvBooks;
     private BookAdapter bookAdapter;
     private BookClient client;
+    private ProgressBar pbSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_list);
         lvBooks = findViewById(R.id.lvBooks);
+        pbSearch = findViewById(R.id.pbSearch);
         ArrayList<Book> aBooks = new ArrayList<>();
         bookAdapter = new BookAdapter(this, aBooks);
         lvBooks.setAdapter(bookAdapter);
-
-        getBooks("Mary Roach");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Get books based on user input
+                getBooks(query);
+                searchView.clearFocus();
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+                searchItem.collapseActionView();
+
+                // Set activity title to search query
+                BookListActivity.this.setTitle(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
         return true;
     }
 
@@ -44,21 +69,21 @@ public class BookListActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
     // Executes an API call to the OpenLibrary search endpoint, parses the results
     // Converts them into an array of book objects and adds them to the adapter
     private void getBooks(String query) {
+        // Show progress bar
+        pbSearch.setVisibility(View.VISIBLE);
         client = new BookClient();
         client.getBooks(query, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
+                    // Hide progress bar
+                    pbSearch.setVisibility(View.GONE);
                     JSONArray docs;
                     if(response != null) {
                         // Get the docs json array
@@ -73,6 +98,7 @@ public class BookListActivity extends AppCompatActivity {
                         bookAdapter.notifyDataSetChanged();
                     }
                 } catch (JSONException e) {
+                    pbSearch.setVisibility(View.GONE);
                     e.printStackTrace();
                 }
             }
