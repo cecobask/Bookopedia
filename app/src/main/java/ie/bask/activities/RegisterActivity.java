eefe0c9914c780229e0ad20ff3564834dbd73c3e
cecobask <32335835+cecobask@users.noreply.github.com>
2019-04-09 14:15:43 +0100

    Updated Firebase query in RegisterActivity.

--- app/src/main/java/ie/bask/activities/RegisterActivity.java
+++ app/src/main/java/ie/bask/activities/RegisterActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import ie.bask.R;
@@ -175,9 +176,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private void loadBooks() {
         app.booksToReadDb = app.usersDb.child(app.firebaseAuth.getCurrentUser().getUid()).child("booksToRead");
         app.bookResultsDb = app.usersDb.child(app.firebaseAuth.getCurrentUser().getUid()).child("bookResults");
+        Query wishlistOrdered = app.booksToReadDb.orderByChild("dateAdded");
 
         // Attaching value event listener
-        app.booksToReadDb.addValueEventListener(new ValueEventListener() {
+        wishlistOrdered.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 // Clearing the previous Books list
